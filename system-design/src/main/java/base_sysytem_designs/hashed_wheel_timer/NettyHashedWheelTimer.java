f2aed0c946b53c999e6c0e4aa851b110e4662bdc
chen tong <chen2015tong@gmail.com>
2024-01-11 19:13:40 +0100

    add system design interivews

---
+++ system-design/src/main/java/base_sysytem_designs/hashed_wheel_timer/NettyHashedWheelTimer.java
@@ -0,0 +1,6 @@
+package base_sysytem_designs.hashed_wheel_timer;
+
+// Netty对时间轮的实现:
+// https://github.com/netty/netty/blob/4.1/common/src/main/java/io/netty/util/HashedWheelTimer.java
+public class NettyHashedWheelTimer {
+}
