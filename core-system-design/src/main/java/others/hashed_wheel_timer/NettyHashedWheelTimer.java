6a10934bf5df9a34f8a98ea342e6cac8fe907b2f
tchen <tong.chen@lexisnexisrisk.com>
2024-06-20 16:39:57 +0200

    update project structure and software dev

---
+++ core-system-design/src/main/java/others/hashed_wheel_timer/NettyHashedWheelTimer.java
@@ -0,0 +1,6 @@
+package others.hashed_wheel_timer;
+
+// Netty对时间轮的实现:
+// https://github.com/netty/netty/blob/4.1/common/src/main/java/io/netty/util/HashedWheelTimer.java
+public class NettyHashedWheelTimer {
+}
