21fab218c9615d80ec4213821bf0f4dbb476157e
chen tong <chen2015tong@gmail.com>
2024-08-12 19:07:48 +0200

    finish url shortening design

--- core-system-design/src/main/java/hashed_wheel_timer/NettyHashedWheelTimer.java
+++ core-system-design/src/main/java/hashed_wheel_timer/NettyHashedWheelTimer.java
@@ -1,4 +1,4 @@
-package others.hashed_wheel_timer;
+package hashed_wheel_timer;
 
 // Netty对时间轮的实现:
 // https://github.com/netty/netty/blob/4.1/common/src/main/java/io/netty/util/HashedWheelTimer.java
